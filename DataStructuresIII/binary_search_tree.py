36ab2468f50c1782f839b08c31bfd8b4f6b09fee
Jisha Obukwelu <jisha@obukwelu.com>
2020-09-21 23:04:44 -0400

    Psuedo code for BST

--- DataStructuresIII/binary_search_tree.py
+++ DataStructuresIII/binary_search_tree.py
@@ -1,7 +1,7 @@
 """
-Binary search trees are a data structure that enforce an ordering over 
-the data they store. That ordering in turn makes it a lot more efficient 
-at searching for a particular piece of data in the tree. 
+Binary search trees are a data structure that enforce an ordering over
+the data they store. That ordering in turn makes it a lot more efficient
+at searching for a particular piece of data in the tree.
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
    on the BSTNode class.
@@ -9,6 +9,11 @@ This part of the project comprises two days:
    on the BSTNode class.
 """
 class BSTNode:
+    """
+    The left subtree of a node contains only nodes with values lesser than the node's values
+    The right subtree of a node contains only nodes with values greater or equal to than the node's values
+    The left and right subtree each must also be a binary search tree.
+    """
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -16,19 +21,70 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
+        # check if the new nodes value is less than the current nodes value
+            # if there is no left child already there
+                # add the new node to the left
+                # create a BSTNode and encapsulate the value in it then set it to the left
+            # otherwise call insert on the left node
+        # otherwise (the new nodes value is greater than or equal to the current nodes value)
+            # if there is no right child already there
+                # add the new node to the right
+                # create a BSTNode and encapsulate the value in it then set it to the right
+            # otherwise call insert on the left node
         pass
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        # if the value of the current node matches the target
+            # return True
+        # check if the target is less than the current nodes value
+            # if there is no left child already there
+                # return False
+            # otherwise
+                # return a call of contains on the left child passing in the target value
+        # otherwise (the target is greater thanthe current nodes value)
+            # if there is no right child already there
+                # return False
+            # otherwise
+                # return a call of contains on the right child passing in the target value
         pass
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # check for an empty tree
+            # return None
+
+        # recursive approach (use more memory)
+        # check if there is no node to the right
+            # return the nodes value
+        # return a call to get max on the right child
+        # -------------------------------------------
+        # iterative approach (more lines of code)
+        # doubly linked list max function
+
+        # initialize max value
+
+        #  get reference to the current node
+
+        # lopp while there is still a current node
+            # if the current value is greater than the max value, update the max value
+            # move on to the next right node
+
+        # return max value
         pass
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        # call back function
+        # call the function - passing the current node's value
+
+        # if there is a node to the left
+            # call the function on the left value
+
+        # if there is a node to the right
+            # call the function on the right node
         pass
 
     # Part 2 -----------------------
---
+++ notes/day3_notes.md
@@ -0,0 +1,22 @@
+# Binary Search Tree
+- https://www.geeksforgeeks.org/binary-search-tree-data-structure/
+
+![binary_search_tree](images/binary_search_tree.png)
+
+Binary Search Tree \
+is a node based binary tree data structure whcih has the following properties:
+- The left subtree of a node contains only nodes with keys lesser than the node's key
+- The right subtree of a node contains only nodes with keys greater than the node's keys
+- The left and right subtree each must also be a binary search tree.
+
+https://www.cs.usfca.edu/~galles/visualization/BST.html
+
+Insert
+- check if the new nodes value is less than the current nodes value
+- if there is no left child already there
+- add the new node to the left
+- otherwise call insert on the left node
+- otherwise (the new nodes value is greater than or equal to the current node value)
+- if there is no right child already there
+- add the new node to the right
+- otherwise call insert on the left node
