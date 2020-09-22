79ddce0fd139ceddba380b16bdea7bbaa3483b71
Jisha Obukwelu <jisha@obukwelu.com>
2020-09-21 23:12:13 -0400

    Add notes to binary search tree

--- DataStructuresII/doubly_linked_list.py
+++ DataStructuresII/doubly_linked_list.py
@@ -8,6 +8,25 @@ class ListNode:
         self.value = value
         self.next = next
 
+   """Wrap the given value in a ListNode and insert it
+    after this node. Note that this node could already
+    have a next node it is point to."""
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+
 """
 Our doubly-linked list class. It holds references to
 the list's head and tail nodes.
